6e761447f6e3390c1531829bd590064e62ab6f19
omnia tawfek <omniatawfek@std.mans.edu.eg>
2025-09-24 17:57:33 +0300

    Impl StudentRepo addStudent

--- repo/repo.cpp
+++ repo/repo.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
+#include "student.h"
 using namespace std;
 
 ///////////////////////////// Student ////////////////////////
 // interface student
 class studentRepo {
-
+public :
+	int virtual addStudent(student student);
 };
 // impelmentation student
 class studentRepoIml : public studentRepo {
-
+private : 
+	student students[25];
+	int index = 0;
+public :
+	int virtual addStudent(student student) {
+		if (index == 25) {
+			cout << "full student";
+		}
+		else
+		{
+			students[index] = student;
+			index++;
+		}
+	}
 };
 
 ///////////////////////////// Teacher ////////////////////////

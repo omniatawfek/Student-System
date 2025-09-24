8c1b7428396b2f42ad6bdf22149c6f1d56a553f0
omnia tawfek <omniatawfek@std.mans.edu.eg>
2025-09-24 18:53:03 +0300

    CourseRepositoryImpl - TeacherRepositoryImpl (add function)

--- repo/repo.cpp
+++ repo/repo.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include "student.h"
+#include "course.h"
+#include "teacher.h"
 using namespace std;
 
 ///////////////////////////// Student ////////////////////////
 // interface student
 class studentRepo {
 public :
-	int virtual addStudent(student student);
+	int virtual addStudent(student student) = 0;
 };
 // impelmentation student
 class studentRepoIml : public studentRepo {
@@ -29,19 +31,48 @@ public :
 ///////////////////////////// Teacher ////////////////////////
 // interface teacher
 class teacherRepo {
-
+public:
+	int virtual addTeacher(teacher teacher) = 0;
 };
 // impelmentation teacher
 class teacherRepoIml : public teacherRepo {
-
+private:
+	teacher teachers[25];
+	int index = 0;
+public:
+	int virtual addTeacher(teacher teacher) {
+		if (index == 25) {
+			cout << "full teacher";
+		}
+		else
+		{
+			teachers[index] = teacher;
+			index++;
+		}
+	}
 };
 
 ///////////////////////////// course ////////////////////////
 // interface course
 class courseRepo {
-
+public:
+	int virtual addCourse(course course) = 0;
 };
 // impelmentation course
 class courseRepoIml : public courseRepo {
+private:
+	course courses[25];
+	int index = 0;
+public:
+	int virtual addCourse(course course) {
+		if (index == 25) {
+			cout << "full Courses";
+		}
+		else
+		{
+			courses[index] = course;
+			index++;
+		}
+	}
 
 };

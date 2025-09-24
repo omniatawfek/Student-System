01ea9b4de06adf05b2f40f369801eb0901c4566a
omnia tawfek <omniatawfek@std.mans.edu.eg>
2025-09-24 17:31:16 +0300

    create repo (student,teacher,course)

---
+++ repo/repo.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+///////////////////////////// Student ////////////////////////
+// interface student
+class studentRepo {
+
+};
+// impelmentation student
+class studentRepoIml : public studentRepo {
+
+};
+
+///////////////////////////// Teacher ////////////////////////
+// interface teacher
+class teacherRepo {
+
+};
+// impelmentation teacher
+class teacherRepoIml : public teacherRepo {
+
+};
+
+///////////////////////////// course ////////////////////////
+// interface course
+class courseRepo {
+
+};
+// impelmentation course
+class courseRepoIml : public courseRepo {
+
+};

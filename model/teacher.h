5bb21723d8973621eaefe72f4d9ee9423b5f1f93
omnia tawfek <omniatawfek@std.mans.edu.eg>
2025-09-24 15:20:35 +0300

    relation between teacker and studentIDS

--- model/teacher.h
+++ model/teacher.h
@@ -5,17 +5,26 @@ class teacher : public BaseEntity
 private:
 	
 	double salary;
+	int studentsIDS[5];
 public:
 	// setter
 	
 	void setSalary(double salary) {
 		this->salary = salary;
 	}
-
+	void setStudentsIDS(int studentsIDS[5]) {
+		for (int i = 0; i < 5; i++) {
+			this->studentsIDS[i] = studentsIDS[i];
+		}
+		
+	}
 	// getter
 
 	double getSalary() {
 		return  salary;
+	}
+	int * getStudentsIDS() {
+		return  studentsIDS;
 	}
 };
 
